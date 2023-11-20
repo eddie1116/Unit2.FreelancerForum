e8009aaace1bf70c0b245c292ccf481c139a8247
eddie1116 <eddie1107@Eddie>
2023-11-20 13:22:48 -0500

    Create a Freelance app

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Unit2.FreelancerForum
+A website where freelancer can advertise their price and occupation and where visitors find
+a list of available freelancer and the price for their services. 
+
+building my website according to the user story.
+
+A visitor enters the website and finds a list of available freelancers. Each freelancer has a name, an occupation, and a starting price for their services. They observe on the list Alice, the writer with a starting price of $30, and Bob, who is a teacher, has a starting price of $50.
+
+The visitor also finds a message that displays the average starting price of all the freelancers. In this example, the average starting price is $40.
+
+A few seconds later, a new listing appears for a freelancer named Carol, who is a programmer and has a starting price of $70. The average starting price is updated to $50.
+
+New freelancers continue to appear every few seconds, and the average starting price is updated accordingly.
---
+++ index.js
@@ -0,0 +1,106 @@
+const state = {
+    averageRate: 50,
+    freelancers: [
+        {
+            name: `Alice`,
+            occupation: `Writer`,
+            rate: 30,
+        },
+        {
+            name: `Bob`,
+            occupation: `Teacher`,
+            rate: 50,
+        },
+        {
+            name: `Carol`,
+            occupation: `Programmer`,
+            rate: 70,
+        },
+    ],
+}
+
+function updateAverageRate() {
+    total = 0;
+    
+    for(let i = 0; i < state.freelancers.length; i++) {
+        const currentFreelancer = state.freelancers[i];
+        total += currentFreelancer.rate;
+    }
+
+    state.averageRate = Math.round(total / state.freelancers.length);
+}
+
+function addFreelancerRow(freelancer){
+    state.freelancers.push(freelancer);
+}
+
+function createFreelancerRow(freelancer) {
+    const tableRow = document.createElement(`tr`);
+
+    const nameInfo = document.createElement(`td`);
+    const occupationInfo = document.createElement(`td`);
+    const rateInfo = document.createElement(`td`);
+
+    nameInfo.textContent = freelancer.name;
+    occupationInfo.textContent = freelancer.occupation;
+    rateInfo.textContent = freelancer.rate;
+
+    tableRow.append(nameInfo, occupationInfo, rateInfo);
+
+    return tableRow;
+}
+
+function clearTable() {
+    const table = document.getElementById(`freelancer-table`);
+
+    while(table.childNodes.length) {
+        table.removeChild(table.childNodes[0]);
+    };
+}
+
+
+function render() {
+    //  clears current table and starts a fresh table
+    clearTable();
+
+    const table = document.getElementById(`freelancer-table`);
+
+    for(let i = 0; i < state.freelancers.length; i++) {
+        const currentFreelancer = state.freelancers[i]; 
+        const row = createFreelancerRow(currentFreelancer);
+        table.appendChild(row);
+    }
+
+    // update the average rate after adding new freelancers
+    updateAverageRate();
+
+    const rateText = document.getElementById(`average-rate`);
+    rateText.textContent = `The average starting rate is $${state.averageRate}.`;
+}
+
+render();
+
+const freshNames = [`Super Man`, `Bad Bunny`, `Batman`, `Flash`, `Pikachu`, `Butterfly`, `John Doe`, `Humpty Dumpty`, `Phil`];
+const freshOccupations = [`Super Hero`, `singer`, `Crime Fighter`, `Cross Country`, `Speaker`, `Carpenter`, `Electrician`, `Sitter`, `Bouncer`];
+const freshRates = [0, 1000, 90, 150, 200, 35, 40, 75, 80];
+
+function createRandomFreelancer() {
+    const randomName = freshNames[Math.floor(Math.random() * freshNames.length)];
+    const randomOccupation = freshOccupations[Math.floor(Math.random() * freshOccupations.length)];
+    const randomRate = freshRates[Math.floor(Math.random() * freshRates.length)];
+
+    const freelancer = {
+        name: randomName,
+        occupation: randomOccupation,
+        rate: randomRate,
+    };
+
+    return freelancer;
+}
+
+setInterval(function() {
+    const newFreelancer = createRandomFreelancer();
+    addFreelancerRow(newFreelancer);
+
+    render();
+}, 3000);
